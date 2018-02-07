a17700d2744ab4a6722e2097fbdb53b4efa6c5b0
pm_Vision <pmar116@uw.edu>
2018-02-07 04:00:06 -0800

    Prep for adding settings Menu

--- app/src/main/java/com/example/preston/projectdicaprio/Pacer.java
+++ app/src/main/java/com/example/preston/projectdicaprio/Pacer.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class Pacer extends AppCompatActivity {
 
@@ -17,11 +18,26 @@ public class Pacer extends AppCompatActivity {
     }
 
     //menu icons are inflated
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         //inflate the menu. This adds items to the action bar, if it is present
         getMenuInflater().inflate(R.menu.menu, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    //Overflow menu button selected
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 }

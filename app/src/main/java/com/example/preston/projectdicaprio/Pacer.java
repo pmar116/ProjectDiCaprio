44fe2720d70c78d1a074d5c46617e84211e31402
pm_Vision <pmar116@uw.edu>
2018-02-07 03:57:27 -0800

    Added Overflow menu for settings

--- app/src/main/java/com/example/preston/projectdicaprio/Pacer.java
+++ app/src/main/java/com/example/preston/projectdicaprio/Pacer.java
@@ -2,6 +2,8 @@ package com.example.preston.projectdicaprio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class Pacer extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class Pacer extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pacer);
+
+        Toolbar myToolbar = (Toolbar)findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    //menu icons are inflated
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //inflate the menu. This adds items to the action bar, if it is present
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }

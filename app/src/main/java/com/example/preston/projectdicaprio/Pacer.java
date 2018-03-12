6648e7b7dc01b4fc01080af7912f1a1d5f9004c6
pm_Vision <pmar116@uw.edu>
2018-03-12 01:39:53 -0700

    tried to make the graph update in real time

--- app/src/main/java/com/example/preston/projectdicaprio/Pacer.java
+++ app/src/main/java/com/example/preston/projectdicaprio/Pacer.java
@@ -1,6 +1,8 @@
 package com.example.preston.projectdicaprio;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -14,33 +16,43 @@ import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 
 public class Pacer extends AppCompatActivity {
+    private final Handler handler = new Handler();
+    private GraphView graph;
+    private Runnable timer1;
+    private LineGraphSeries<DataPoint> series;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //creation of instance
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pacer);
 
+        //toolkit stuff
         Toolbar myToolbar = (Toolbar)findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
+        //Create the circle and run the animations
         final View circle = findViewById(R.id.pacerCircle);
         final Animation anim = AnimationUtils.loadAnimation(this, R.anim.scale);
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3),
-                new DataPoint(3, 2),
-                new DataPoint(4, 6)
-        });
+
+        //Graph init
+        graph = (GraphView) findViewById(R.id.graph);
+        series = new LineGraphSeries<>(generateData());
         graph.addSeries(series);
+
+
+
         Button start = (Button) findViewById(R.id.startButton);
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                    circle.startAnimation(anim);
+                circle.startAnimation(anim);
 
             }
         });
@@ -69,6 +81,53 @@ public class Pacer extends AppCompatActivity {
                 // Invoke the superclass to handle it.
                 return super.onOptionsItemSelected(item);
 
+        }
+    }
+
+    //when the graph resumes
+    @Override
+    public void onResume(){
+        super.onResume();
+        timer1 = new Runnable(){
+            @Override
+            public void run(){
+                series.resetData(generateData());
+                handler.postDelayed(this,781);
+            }
+        };
+        handler.postDelayed(timer1, 781);
+    }
+
+    //when the graph pauses
+    @Override
+    public void onPause(){
+        handler.removeCallbacks(timer1);
+        super.onPause();
+    }
+
+
+    private DataPoint[] generateData(){
+        //generate data based on text file
+        DataPoint[] output = new DataPoint[370];
+        int x;
+        int y;
+        try{
+            Resources res = getResources();
+            InputStream input = res.openRawResource(R.raw.test1);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+            String data;
+            for(int i = 0; i < output.length; i++){
+                data = reader.readLine();
+                String[] elements = data.split(" ");
+                x = Integer.parseInt(elements[0]);
+                y = Integer.parseInt(elements[1]);
+                DataPoint v = new DataPoint(x,y);
+                output[i] = v;
+            }
+            return output;
+        }catch(Exception e){
+            //can't show file
+            return null;
         }
     }
 }
